baed10c1e2283dc09403c422b176f70baa2c92e4
liamkee <lykee11199@gmail.com>
2026-05-02 12:15:05 +0800

    feat:local db setup with drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   out: './migrations',
   dialect: 'sqlite',
+  dbCredentials: {
+    url: process.env.LOCAL_DB_PATH || 'fallback.sqlite',
+  },
 });
